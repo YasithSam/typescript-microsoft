7aff308c00d5ad7f56bf8acba1c7c127532f29f8
YasithSam <yasith.sam7@gmail.com>
2021-12-10 10:49:36 +0530

    add

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+function add(x: number, y: number){
+    return x+y
+  }
+const result=add(5,7);
+console.log(result)
---
+++ src/types.ts
@@ -0,0 +1,93 @@
+// Auto filtering
+// Function with type cast
+
+// Reverse engineer for browsers
+// React
+// Npx create-react-app name --template typescript
+
+
+// primitive types
+// number, string, array , boolean
+// any -> can be assigned to anything
+
+// let name= "sam";
+// let pets: string[] = ["hey","hey"];
+// pets.push()
+
+// interface Customer{
+//     firstName: string;
+//     lastName: string;
+// }
+// let customer : Customer = {
+//   firstName="sam",
+//   lastName="sam2"
+// }
+
+
+
+// class Customer{
+//     firstName ='';
+//     lastName='';
+//     age: number=0;
+
+//     constructor(firstName: string, lastName: string ,age: number){
+
+//     }
+//     or
+//     constructor(fname: string,lname: string){
+//         this.firstName=fname;
+//     }
+// }
+
+
+// class Customer{
+//     placeOrder(prodcuctId: number){
+//         console.log(prodcuctId)
+//     }
+//     constructor(
+//         public firstname: string,
+//         public lastname: string
+
+//     ){}
+// }
+
+// inheritence
+
+// class GoldCustomer extends Customer{
+    // constructor(){
+    //     supper('','',0)
+    // }
+// }
+
+
+// interface Settings{
+//     title: string;
+//     color: string
+// }
+
+// function initalize(settings : Settings){
+
+// }
+
+// initalize({
+//   title: "name",
+//   color:"blue"
+// });
+
+// class Customer implements Settings{
+//     //should have contract
+// }
+
+// inetrafces are used in json returns and sends
+
+
+// generics
+// let pets: string[]=[]; // Array<string>
+
+
+// function test<T>(x: T,y: T){
+
+// }
+
+// test<number>(2,3);
+// test<string>("3","2");
